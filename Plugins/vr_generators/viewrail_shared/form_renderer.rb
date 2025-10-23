64befa7c5b6b29305eb4efc3fff85c6269024f3e
BenGE-Viewrail <ben.goodelliott@viewrail.com>
2025-10-23 08:52:38 -0400

    began to reimplement locking fields to the stair forms

--- Plugins/vr_generators/viewrail_shared/form_renderer.rb
+++ Plugins/vr_generators/viewrail_shared/form_renderer.rb
@@ -1,3 +1,4 @@
+require 'base64'
 module Viewrail
 
   module SharedUtilities
@@ -5,7 +6,30 @@ module Viewrail
     class FormRenderer
 
       def initialize(values)
-        @last_values = values
+        @last_values = values  # ERB templates expect @last_values
+        load_icon_data_uris
+      end
+
+      def load_icon_data_uris
+        base_dir = File.dirname(__FILE__)
+        icons_dir = File.join(base_dir, '..', 'stair_generator', 'icons')
+
+        unlocked_path = File.join(icons_dir, 'padlock_unlocked.svg')
+        locked_path = File.join(icons_dir, 'padlock_locked.svg')
+
+        if File.exist?(unlocked_path) && File.exist?(locked_path)
+          unlocked_svg = File.read(unlocked_path)
+          locked_svg = File.read(locked_path)
+
+          # Convert to data URIs
+          @unlocked_icon = "data:image/svg+xml;base64,#{Base64.strict_encode64(unlocked_svg)}"
+          @locked_icon = "data:image/svg+xml;base64,#{Base64.strict_encode64(locked_svg)}"
+        else
+          puts "Warning: Could not find lock icon files"
+          puts "Looked in: #{icons_dir}"
+          @unlocked_icon = ""
+          @locked_icon = ""
+        end
       end
 
       def render(template_path)
